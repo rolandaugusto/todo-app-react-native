20efcaa78e4a5af69e0fa79993871e6b31d09cda
rolandaugusto <rolandaugusto@gmail.com>
2023-07-24 18:58:03 +0200

    initial commit

--- App.js
+++ App.js
@@ -1,20 +1,84 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from "react";
+import {
+  Button,
+  FlatList,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from "react-native";
+import uuid from "react-native-uuid";
 
 export default function App() {
+  const [todos, setTodos] = useState([]);
+  const [inputText, setInputText] = useState("");
+
+  const handleTextInput = (value) => {
+    setInputText(value);
+  };
+  const handleButtonAddPress = () => {
+    setTodos((currentTodos) => [
+      ...currentTodos,
+      { text: inputText, key: uuid.v4() },
+    ]);
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.textInput}
+          placeholder="New Todo"
+          onChangeText={handleTextInput}
+        />
+        <Button title="Add" onPress={handleButtonAddPress} />
+      </View>
+      <View style={styles.todosContainer}>
+        <FlatList
+          data={todos}
+          renderItem={(itemData) => {
+            return (
+              <View style={styles.todoWrapper}>
+                <Text style={styles.todoText}>{itemData.item.text}</Text>
+              </View>
+            );
+          }}
+        ></FlatList>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
+    paddingHorizontal: 16,
+    paddingTop: 48,
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  inputContainer: {
+    justifyContent: "space-between",
+    flexDirection: "row",
+    marginBottom: 16,
+    marginTop: 30,
+  },
+  textInput: {
+    flex: 1,
+    borderWidth: 1,
+    marginRight: 8,
+    borderColor: "#ccc",
+    padding: 8,
+  },
+  todosContainer: {
+    flex: 4,
+    marginBottom: 16,
+  },
+  todoWrapper: {
+    borderRadius: 4,
+    backgroundColor: "#eee",
+    marginBottom: 4,
+  },
+  todoText: {
+    color: "#222",
+    padding: 8,
   },
 });
